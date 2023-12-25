05b7a1f7cf61825569f1e4c88fc24a00a3182cde
sikwan <yysskk99999@naver.com>
2023-12-25 23:15:07 +0900

    first

---
+++ YangFinTalk/YangFinTalk/YangFinTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  YangFinTalkApp.swift
+//  YangFinTalk
+//
+//  Created by 양시관 on 2023/12/25.
+//
+
+import SwiftUI
+
+@main
+struct YangFinTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
