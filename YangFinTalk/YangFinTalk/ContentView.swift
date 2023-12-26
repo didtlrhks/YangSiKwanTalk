7a8dd8dfffd715f66319c2c267504282335abd3b
sikwan <yysskk99999@naver.com>
2023-12-26 18:36:04 +0900

    Creating a Login Page

--- YangFinTalk/YangFinTalk/ContentView.swift
+++ YangFinTalk/YangFinTalk/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isLoginMode = false
+    @State var Email = ""
+    @State var password = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ScrollView{
+                
+                VStack{
+                    Picker(selection: $isLoginMode  , label: Text("Picker here")){
+                        Text("Login")
+                            .tag(true)
+                        Text("create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                    
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size:64))
+                            .padding()
+                    }
+                    TextField("Email", text: $Email)
+                    TextField("password", text: $password)
+                    
+                    Button{
+                        
+                    } label: {
+                        HStack{
+                            Spacer()
+                            Text("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical,10)
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                 
+                    Text("로그인 페이지")
+                
+                }.padding()
         }
-        .padding()
+        .navigationTitle("Create Account")
+        
+    }
     }
 }
 
