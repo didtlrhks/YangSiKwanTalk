55eb0376f5a8f7a51fd75f00a464f54b83ed05d1
sikwan <yysskk99999@naver.com>
2023-12-29 18:30:28 +0900

    MainMessagesView and firebase current User manage

--- YangFinTalk/YangFinTalk/MainMessagesView.swift
+++ YangFinTalk/YangFinTalk/MainMessagesView.swift
@@ -7,24 +7,58 @@
 
 import Foundation
 import SwiftUI
+import SDWebImageSwiftUI
+
+struct ChatUser {
+    let uid, email,profileImageUrl : String
+    
+}
 
 class MainMessagesViewModel : ObservableObject {
+    
+    @Published var errorMessage = ""
+    @Published var chatUser : ChatUser?
     
     init() {
         fetchCurrentUser()
         
     }
     private func fetchCurrentUser() {
+       
         guard let uid =
         FirebaseManager.shared.auth
-            .currentUser?.uid else {return}
+            .currentUser?.uid else {
+            self.errorMessage = "Could not find firebase uid"
+            return
+        }
+     
         
         FirebaseManager.shared.firestore.collection("users")
             .document(uid).getDocument {
                 snapshot, error in
                 if let error = error{
-                    print("Failed to fetch current ")
+                    self.errorMessage = "Failed to fetch current user:\(error)"
+                    print("Failed to fetch current user:" , error)
+                    return
                 }
+                
+                self.errorMessage = "123"
+                
+                guard let data = snapshot?.data() else{
+                    self.errorMessage = "No data found"
+                    return
+                }
+//                print(data)
+                
+              //  self.errorMessage = "Data : \(data.description)"
+                
+                let uid = data["uid"] as? String ?? ""
+                let email = data["email"] as? String ?? ""
+                let profileImageUrl = data["profileImageUrl"] as? String ?? ""
+                self.chatUser = ChatUser(uid: uid, email: email, profileImageUrl: profileImageUrl)
+                
+               // self.errorMessage = chatUser.profileImageUrl
+                
             }
     }
     
@@ -34,15 +68,44 @@ struct MainMessagesView: View {
     
     @State var shouldShowLogOutOptions = false
     
+    @ObservedObject private var vm = MainMessagesViewModel()
+    
+    var body: some View {
+        NavigationView {
+            
+            VStack {
+                
+              //  Text("User: \(vm.chatUser?.uid ?? "")")
+                customNavBar
+                messagesView
+            }
+            .overlay(
+                newMessageButton, alignment: .bottom)
+            .navigationBarHidden(true)
+        }
+    }
+    
     private var customNavBar: some View {
         HStack(spacing: 16) {
             
-            Image(systemName: "person.fill")
-                .font(.system(size: 34, weight: .heavy))
+            WebImage(url : URL(string: vm.chatUser?.profileImageUrl ?? ""))
+                .resizable()
+                .scaledToFill()
+                .frame(width: 50,height:  50)
+                .clipped()
+                .cornerRadius(50)
+                .overlay(RoundedRectangle(cornerRadius : 44)
+                    .stroke(Color(.label),
+                           lineWidth:  1
+                           )
+                )
+                .shadow(radius: 5)
+
             
             VStack(alignment: .leading, spacing: 4) {
-                Text("USERNAME")
-                    .font(.system(size: 24, weight: .bold))
+                           let email = vm.chatUser?.email.replacingOccurrences(of: "@gmail.com", with: "") ?? ""
+                           Text(email)
+                               .font(.system(size: 24, weight: .bold))
                 
                 HStack {
                     Circle()
@@ -75,18 +138,7 @@ struct MainMessagesView: View {
         }
     }
     
-    var body: some View {
-        NavigationView {
-            
-            VStack {
-                customNavBar
-                messagesView
-            }
-            .overlay(
-                newMessageButton, alignment: .bottom)
-            .navigationBarHidden(true)
-        }
-    }
+
     
     private var messagesView: some View {
         ScrollView {
